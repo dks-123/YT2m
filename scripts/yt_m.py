73695a128e47610ae94b3ecd54126e2f65fb96a4
dks-123 <dkshods@gmail.com>
2025-02-18 16:14:16 +0800

    Update yt_m.py

--- scripts/yt_m.py
+++ scripts/yt_m.py
@@ -30,7 +30,7 @@ os.makedirs(output_dir, exist_ok=True)
 
 def grab(youtube_url):
     """使用 yt-dlp 解析 M3U8 連結"""
-    yt_dlp_cmd =f‘yt-dlp "bestaudio[protocol=m3u8]+bestvideo[protocol=m3u8]" --geo-bypass --cookies cookies.txt --sleep-requests 1 --limit-rate 1M --retries 5 --fragment-retries 10 --no-warnings --quiet --no-check-certificate --no-playlist {youtube_url}’
+    yt_dlp_cmd = f"yt-dlp --geo-bypass --cookies cookies.txt --sleep-requests 1 --limit-rate 1M --retries 5 --fragment-retries 5 --no-warnings --quiet --no-check-certificate --no-playlist -g {youtube_url}"
     try:
         result = subprocess.run(yt_dlp_cmd, shell=True, capture_output=True, text=True, check=True)
         m3u8_url = result.stdout.strip()
